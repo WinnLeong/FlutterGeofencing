5d83a2d0ef3cc6fe5585e50113d183c31d3d969d
WinnLeong <wl.ong@outlook.com>
2021-04-24 13:08:45 +0800

    Migrate to null safety

--- lib/src/callback_dispatcher.dart
+++ lib/src/callback_dispatcher.dart
@@ -18,9 +18,9 @@ void callbackDispatcher() {
   _backgroundChannel.setMethodCallHandler((MethodCall call) async {
     final List<dynamic> args = call.arguments;
     final Function callback = PluginUtilities.getCallbackFromHandle(
-        CallbackHandle.fromRawHandle(args[0]));
+        CallbackHandle.fromRawHandle(args[0]))!;
     assert(callback != null);
-    final List<String> triggeringGeofences = args[1].cast<String>();
+    final List<String>? triggeringGeofences = args[1].cast<String>();
     final List<double> locationList = <double>[];
     // 0.0 becomes 0 somewhere during the method call, resulting in wrong
     // runtime type (int instead of double). This is a simple way to get
